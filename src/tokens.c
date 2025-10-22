465600d7b175c69e8597aac86f618f039673e05a
Tjark Krueger <krugertjark@gmail.com>
2025-10-22 17:49:01 +0200

    missed a \n whoopsie

--- src/tokens.c
+++ src/tokens.c
@@ -15,7 +15,7 @@ void    what_to_do_with_tokens(char **tokens)
     if (strcmp(tokens[0], "echo") == 0)
         run_echo(tokens);
     else 
-        printf("please put real command i can handle stupid");
+        printf("please put real command i can handle stupid\n");
     //else if ()
     //else if ()
     //... all other simple inbuild command the others are coming in another function for reasons like need also multiple arguments and stuff
