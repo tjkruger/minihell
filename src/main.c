a5799aab75813f00922d913025aa5eb4b1831968
tjkruger <krugertjark@gmail.com>
2025-12-12 14:37:20 +0100

    for test purposes

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: tjkruger <tjkruger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: Invalid date        by                   #+#    #+#             */
-/*   Updated: 2025/12/12 14:09:46 by tjkruger         ###   ########.fr       */
+/*   Updated: 2025/12/12 14:37:06 by tjkruger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -232,7 +232,7 @@ int main(int argc, char **argv, char **env)
 			printf("%d\n", exit_status);
 		}
 		//print_everything(token_list, cmds, history_list);//for now to test
-        //print_tokens(token_list);
+        print_tokens(token_list);
 		free(input);
 		free_cmd_list(cmds);
 		cmds = NULL;
