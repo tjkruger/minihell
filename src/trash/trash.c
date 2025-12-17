c6da2aa10985bd57183551b9b7f355ff5d2c62ea
Tjark Krueger <krugertjark@gmail.com>
2025-12-17 23:08:45 +0100

    fixed a segfault ... got hit by another ...

--- src/trash/trash.c
+++ src/trash/trash.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   trash.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hkaraogl <hkaraogl@student.42heilbronn.    +#+  +:+       +#+        */
+/*   By: r2d2 <r2d2@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/16 16:43:31 by hkaraogl          #+#    #+#             */
-/*   Updated: 2025/12/17 16:23:15 by hkaraogl         ###   ########.fr       */
+/*   Updated: 2025/12/17 23:04:56 by r2d2             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,164 +14,161 @@
 
 void trash_init(t_trash *trash)
 {
-	trash->head = NULL;
-	trash->tail = NULL;
-	trash->size = 0;
+    if (!trash)
+        return;
+    trash->head = NULL;
+    trash->tail = NULL;
+    trash->size = 0;
 }
 
+/* add pointer to GC list; safe if trash is NULL (just don't track) */
 void gc_add(t_trash *trash, void *ptr)
 {
-	t_trash_node *new_node;
+    t_trash_node *new_node;
 
-	if(!trash || !ptr)
-		return;
-	new_node = malloc(sizeof(t_trash_node));
-	if(!new_node)
-	{
-		free(ptr);
-		ft_perror("gc_add: malloc failed");
-		return;	
-	}
-	new_node->data = ptr;
-	new_node->next = NULL;
-	if(!trash->head)
-	{
-		trash->head = new_node;
-		trash->tail = new_node;
-	}
-	else
-	{
-		trash->tail->next = new_node;
-		trash->tail = new_node;
-	}
-	trash->size++;
+    if (!ptr)
+        return;
+    if (!trash)
+        return; /* not tracking if trash is NULL */
+
+    new_node = malloc(sizeof(t_trash_node));
+    if (!new_node)
+    {
+        free(ptr);
+        perror("gc_add: malloc failed");
+        return;
+    }
+    new_node->data = ptr;
+    new_node->next = NULL;
+    if (!trash->head)
+    {
+        trash->head = new_node;
+        trash->tail = new_node;
+    }
+    else
+    {
+        trash->tail->next = new_node;
+        trash->tail = new_node;
+    }
+    trash->size++;
 }
 
 void *gc_malloc(t_trash *trash, size_t count, size_t size)
 {
-	void *ptr;
+    void *ptr;
 
-	ptr = malloc(count * size);
-	if(!ptr)
-	{
-		ft_perror("malloc failed");
-		return NULL;
-	}
-	gc_add(trash, ptr);
-	return ptr;
+    if (count == 0 || size == 0)
+        return NULL;
+    ptr = malloc(count * size);
+    if (!ptr)
+    {
+        perror("malloc failed");
+        return NULL;
+    }
+    gc_add(trash, ptr);
+    return ptr;
 }
 
 char *gc_strdup(t_trash *trash, const char *s1)
 {
-	int		len;
-	int		count;
-	char	*new_str;
+    size_t  len;
+    char    *new_str;
 
-	len = 0;
-	count = 0;
-	while (s1[len] != '\0')
-		len++;
-	new_str = (char *)gc_malloc(trash,len + 1, sizeof(char));
-	if (new_str == 0)
-	{
-		return (0);
-	}
-	while (count < len)
-	{
-		new_str[count] = s1[count];
-		count++;
-	}
-	new_str[len] = '\0';
-	return (new_str);
+    if (!s1)
+    {
+        /* return empty string tracked by GC */
+        new_str = gc_malloc(trash, 1, sizeof(char));
+        if (!new_str)
+            return NULL;
+        new_str[0] = '\0';
+        return new_str;
+    }
+    len = ft_strlen(s1);
+    new_str = gc_malloc(trash, len + 1, sizeof(char));
+    if (!new_str)
+        return NULL;
+    ft_memcpy(new_str, s1, len);
+    new_str[len] = '\0';
+    return new_str;
 }
 
 char *gc_substr(t_trash *trash, const char *s, unsigned int start, size_t len)
 {
-	char	*temp;
-	size_t	i;
-	size_t	len_s;
+    char    *temp;
+    size_t  len_s;
 
-	temp = NULL;
-	i = 0;
-	if (!s)
-		return (NULL);
-	len_s = ft_strlen(s);
-	if (start >= len_s)
-		return (ft_strdup(""));
-	if (len > len_s - start)
-		len = len_s - start;
-	temp =gc_malloc(trash, len + 1,  sizeof(char));
-	if (!temp)
-		return (NULL);
-	while (i < len)
-	{
-		temp[i] = s[start + i];
-		i++;
-	}
-	temp[i] = '\0';
-	return (temp);
+    if (!s)
+        return gc_strdup(trash, "");
+    len_s = ft_strlen(s);
+    if (start >= len_s)
+        return gc_strdup(trash, "");
+    if (len > len_s - start)
+        len = len_s - start;
+    temp = gc_malloc(trash, len + 1, sizeof(char));
+    if (!temp)
+        return NULL;
+    ft_memcpy(temp, s + start, len);
+    temp[len] = '\0';
+    return temp;
 }
 
 void gc_cleanup(t_trash *trash)
 {
-	t_trash_node *current;
-	t_trash_node *next;
+    t_trash_node *current;
+    t_trash_node *next;
 
-	if (!trash)
-		return;
-	current = trash->head;
-	while (current)
-	{
-		next = current->next;
-		free(current->data);
-		free(current);
-		current = next;
-	}
-	trash->head = NULL;
-	trash->tail = NULL;
-	trash->size = 0;
+    if (!trash)
+        return;
+    current = trash->head;
+    while (current)
+    {
+        next = current->next;
+        if (current->data)
+            free(current->data);
+        free(current);
+        current = next;
+    }
+    trash->head = NULL;
+    trash->tail = NULL;
+    trash->size = 0;
 }
 
 void gc_print(t_trash *trash)
 {
-	t_trash_node *current;
-	size_t index = 0;
+    t_trash_node *current;
+    size_t index = 0;
 
-	if (!trash)
-		return;
-	current = trash->head;
-	printf("Garbage Collector Contents (size: %zd):\n", trash->size);
-	while (current)
-	{
-		printf(" [%zu]: %p\n", index, current->data);
-		current = current->next;
-		index++;
-	}
+    if (!trash)
+        return;
+    current = trash->head;
+    printf("Garbage Collector Contents (size: %zd):\n", trash->size);
+    while (current)
+    {
+        printf(" [%zu]: %p\n", index, current->data);
+        current = current->next;
+        index++;
+    }
 }
 
 char *gc_strjoin(t_trash *trash, char const *s1, char const *s2)
 {
-	char	*temp;
-	size_t	len;
-	size_t	i;
+    size_t  len1;
+    size_t  len2;
+    char    *out;
+    size_t  i;
 
-	i = 0;
-	if (!s1 || !s2)
-		return (NULL);
-	len = ft_strlen(s1) + ft_strlen(s2);
-	temp = gc_malloc(trash, (len + 1), sizeof(char));
-	if (!temp)
-		return (NULL);
-	while (s1[i] != '\0')
-	{
-		temp[i] = s1[i];
-		i++;
-	}
-	while (s2[i - ft_strlen(s1)] != '\0')
-	{
-		temp[i] = s2[i - ft_strlen(s1)];
-		i++;
-	}
-	temp[i] = '\0';
-	return (temp);
+    if (!s1 || !s2)
+        return NULL;
+    len1 = ft_strlen(s1);
+    len2 = ft_strlen(s2);
+    out = gc_malloc(trash, len1 + len2 + 1, sizeof(char));
+    if (!out)
+        return NULL;
+    i = 0;
+    if (len1)
+        ft_memcpy(out + i, s1, len1), i += len1;
+    if (len2)
+        ft_memcpy(out + i, s2, len2), i += len2;
+    out[i] = '\0';
+    return out;
 }
