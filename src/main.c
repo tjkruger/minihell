f828a39dc98f28f390d7f264d2269ad35cd82b01
Tjark Krueger <krugertjark@gmail.com>
2026-01-07 00:35:50 +0100

    added everything for working signals so that should be fine now in the lower shlvl need the tracker but other then that its fine

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tjkruger <tjkruger@student.42.fr>          +#+  +:+       +#+        */
+/*   By: r2d2 <r2d2@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: Invalid date        by                   #+#    #+#             */
-/*   Updated: 2026/01/05 17:37:54 by tjkruger         ###   ########.fr       */
+/*   Updated: 2026/01/06 17:07:13 by r2d2             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -187,21 +187,23 @@ int main(int argc, char **argv, char **env)
 	exit_status = 0;
 	ms.all_commands = NULL;
 	ms.env_list = init_environment(env);
-
+	rl_catch_signals = 0;
+	rl_catch_sigwinch = 0;
 	setup_signals_interactive();
 	trash_init(&ms.trash);
+	
 
 	while (1)
 	{
 		input = readline("minisHell> ");
-		if (g_signal_status == 130)
+		if (g_signal_status == SIGINT)
 		{
 			exit_status = 130;
-			ms.env_list->last_exit = exit_status;
+			ms.env_list->last_exit = 130;
 			g_signal_status = 0;
 			if (input)
 				free(input);
-			continue;             // ← Wichtig: Neue Iteration!
+			continue;
 		}
 		if (!input)
 		{
